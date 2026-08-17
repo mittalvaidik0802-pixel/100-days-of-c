8040aa24ff1009e56d30daf1a7caa8c35786cb7a
mittalvaidik0802-pixel <mittalvaidik0802@gmail.com>
2026-08-17 23:48:21 +0530

    Day - 5 completed

---
+++ DAY-5/question9.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float p, r, t, si, ci, amount;
+
+    printf("Enter principal, rate and time: ");
+    scanf("%f %f %f", &p, &r, &t);
+
+    si = (p * r * t) / 100;
+
+    amount = p * pow((1 + r / 100), t);
+    ci = amount - p;
+
+    printf("Simple Interest = %.2f\n", si);
+    printf("Compound Interest = %.2f\n", ci);
+
+    return 0;
+}
