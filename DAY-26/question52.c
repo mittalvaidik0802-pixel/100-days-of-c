983045a96cc6700a55ec20c75faeb39586ed9d6d
mittalvaidik0802-pixel <mittalvaidik0802@gmail.com>
2026-09-15 10:52:28 +0530

    DAY-26 Completed

---
+++ DAY-26/question52.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    for (i = 1; i <= 5; i += 2) {
+        for (j = 1; j <= i; j++) {
+            printf("*\n");
+        }
+        printf("\n");
+    }
+
+    for (i = 3; i >= 1; i -= 2) {
+        for (j = 1; j <= i; j++) {
+            printf("*\n");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
