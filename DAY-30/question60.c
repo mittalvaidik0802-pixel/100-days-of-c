58a1f56daea6cb94a62d17c4d38b4690edfc16b4
mittalvaidik0802-pixel <mittalvaidik0802@gmail.com>
2026-09-15 11:03:45 +0530

    DAY-30 Completed

---
+++ DAY-30/question60.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, arr[100];
+    int positive = 0, negative = 0, zero = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+
+        if (arr[i] > 0)
+            positive++;
+        else if (arr[i] < 0)
+            negative++;
+        else
+            zero++;
+    }
+
+    printf("Positive numbers = %d\n", positive);
+    printf("Negative numbers = %d\n", negative);
+    printf("Zeros = %d", zero);
+
+    return 0;
+}
